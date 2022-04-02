ecae52c988cdd76fb98fb7a8dc73784cb6991fa5
madrimov <madrimov5014>
2022-04-02 21:41:04 +0500

    fetching

--- src/view/components/Sidebar.jsx
+++ src/view/components/Sidebar.jsx
@@ -3,18 +3,16 @@ import { NavLink } from "react-router-dom";
 import { MdAddBusiness, MdViewList } from "react-icons/md";
 
 export const Sidebar = () => {
-	const id = useId()
-	console.log(id);
   return (
     <aside>
       <ul className="menu bg-base-100 p-2 gap-2 rounded-box">
         <li >
-          <NavLink id={id} style={{padding: '10px'}} className="bg-base-200 text-2xl" to={'/'}>
+          <NavLink  style={{padding: '10px'}} className="bg-base-200 text-2xl" to={'/'}>
             <MdAddBusiness />
           </NavLink>
         </li>
         <li >
-          <NavLink id={id} style={{padding: '10px'}} className="bg-base-200 text-2xl" to={'viewlist'}>
+          <NavLink  style={{padding: '10px'}} className="bg-base-200 text-2xl" to={'viewlist'}>
             <MdViewList />
           </NavLink>
         </li>
